12b2b09685537c9e71cce9a75e60f03e03fffb21
radim <monsoon78@volny.cz>
2016-04-22 15:41:00 +0200

    Initial commit
    First synced repository

---
+++ src/javatutorial/Lambda.java
@@ -0,0 +1,46 @@
+package javatutorial;
+
+
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Lambda {
+
+	public static void main(String[] args) {
+		ArrayList<Person> roster;
+		Person p1 = new Person();
+		Person p2 = new Person();
+		
+		roster = new ArrayList<Person>();
+		
+		processPersonsWithFunction(
+			    roster,
+			    p -> p.getGender() == Person.Sex.MALE
+			        && p.getAge() >= 18
+			        && p.getAge() <= 25,
+			    p -> p.getEmailAddress(),
+			    email -> System.out.println(email)
+			);
+	
+				
+
+	}
+	
+	public static void processPersonsWithFunction(ArrayList<Person> roster, Predicate<Person> tester,
+			Function<Person, String> mapper, Consumer<String> block)
+	{
+		for (Person p : roster) {
+		    if (tester.test(p)) {
+		       String data = mapper.apply(p);
+		       block.accept(data);
+		    }
+		}
+	}	
+
+	
+	
+
+}
---
+++ src/javatutorial/Person.java
@@ -0,0 +1,30 @@
+package javatutorial;
+
+import java.time.LocalDate;
+
+import javatutorial.Person.Sex;
+
+public class Person {
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+	public Sex getGender() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public String getEmailAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+     
+
+}
