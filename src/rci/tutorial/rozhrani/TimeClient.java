7b2c16d3398573226615e01334babf74543e29ef
radim <monsoon78@volny.cz>
2016-04-26 17:26:07 +0200

    Rozrhani01

---
+++ src/rci/tutorial/rozhrani/TimeClient.java
@@ -0,0 +1,14 @@
+package rci.tutorial.rozhrani;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by cimorads on 4/26/2016.
+ */
+public interface TimeClient {
+    void setTime(int hour, int minute, int second);
+    void setDate(int day, int month, int year);
+    void setDateAndTime(int day, int month, int year,
+                        int hour, int minute, int second);
+    LocalDateTime getLocalDateTime();
+}
